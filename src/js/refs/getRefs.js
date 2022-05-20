ba3fda7aa634a88c6c5d95fb3322aa6a5a41e06d
AButsai <butsai@ukr.net>
2022-05-20 17:42:55 +0300

    add form login and regictration, html and scss

---
+++ src/js/refs/getRefs.js
@@ -0,0 +1,10 @@
+function getRefs() {
+  return {
+    signIn: document.querySelector('.signin__btn'),
+    signOut: document.querySelector('.signout__btn'),
+    formBox: document.querySelector('.form__box'),
+    wrapperLogin: document.querySelector('.section__login'),
+  };
+}
+
+export default getRefs;
