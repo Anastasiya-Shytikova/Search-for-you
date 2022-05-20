545de44ecb64a6dfdc64ee5f1044d684daab6b19
Anastasiya-Shytikova <91418875+Anastasiya-Shytikova@users.noreply.github.com>
2022-05-20 12:16:18 +0000

    Deploying to gh-pages from @ Anastasiya-Shytikova/search-for-you@4b063fffd9efd275d9a54d4468bb1b436615358e 🚀

--- src.71fb5d2b.js
+++ src.71fb5d2b.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/search-for-you/src.292c24f2.js.map
+//# sourceMappingURL=/search-for-you/src.71fb5d2b.js.map
